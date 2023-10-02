7e2a236273aedfa150f170d174010b53f0da0a2f
chang hee kim <changheekim@changui-MacBookAir.local>
2023-10-02 19:30:38 +0900

    로그인, 토큰확인, 로그아웃 구현

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 const port = 4000;
 // const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key.js');
-
+const { auth } = require('./middleware/auth.js');
 const { User } = require('./models/User.js');
 
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
@@ -25,19 +26,95 @@ app.get('/', (req, res) => {
     res.send('즐거운 추석입니다. 10월 2일');
 });
 
-app.post('/register', (req, res) => {
-    const user = new User(req.body);
+app.post('/api/users/register', async (req, res) => {
+    const user = new User(req.body)
 
-    user.save() // 이 부분에서 콜백 함수를 제거하고 프로미스를 반환하도록 수정
-        .then(userInfo => {
-            return res.status(200).json({
-                success: true,
-            });
+    const result = await user.save().then(() => {
+        res.status(200).json({
+            success: true
         })
-        .catch(err => {
-            return res.json({ success: false, err });
-        });
-});
+    }).catch((err) => {
+        res.json({ success: false, err })
+    })
+})
+
+app.post('/api/users/login', async (req, res) => {
+
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        const isMatch = await user.comparePassword(req.body.password)
+        console.log(isMatch)
+        if (!isMatch) {
+            return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+        }
+
+        const token = await user.generateToken()
+        res.cookie("user_auth", token).status(200).json({ loginSuccess: true, userId: user._id })
+    } catch (err) {
+        return res.status(400).send(err)
+    }
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? true : false,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, async (req, res) => {
+    try {
+        await User.findOneAndUpdate(
+            { _id: req.user._id },
+            { token: '' }
+        )
+
+        return res.status(200).send({
+            success: true
+        })
+    } catch (err) {
+        return res.json({ success: false, err })
+    }
+})
+
+// app.get('/api/users/auth', auth, async (req, res) => {
+//     //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말
+//     res.status(200).json({
+//         _id: req.user._id,
+//         isAdmin: req.user.role === 0 ? false : true,
+//         isAuth: true,
+//         email: req.user.email,
+//         name: req.user.name,
+//         lastname: req.user.lastname,
+//         role: req.user.role,
+//         image: req.user.image
+//     })
+// });
+
+// app.get('/api/users/logout', auth, (req, res) => {
+//     User.findOneAndUpdate({ _id: req.user._id },
+//         { token: "" }
+//         , (err, user) => {
+//             if (err) return res.json({ success: false, err });
+//             return res.status(200).send({
+//                 success: true
+//             })
+//         })
+// })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -29,6 +33,71 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function (next) {
+    let user = this;
+
+    if (user.isModified('password')) {
+        // 비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+
+})
+
+userSchema.methods.comparePassword = function (plainPassword) {
+    console.log(plainPassword)
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(isMatch)
+            }
+        })
+    })
+}
+
+userSchema.methods.generateToken = function () {
+    const user = this
+    const token = jwt.sign({ userId: user._id.toHexString() }, "secretToken")
+
+    user.token = token
+    return user.save()
+        .then(() => token)
+}
+
+
+userSchema.statics.findByToken = function (token) {
+    const user = this
+
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, 'secretToken', (err, decoded) => {
+            if (err) {
+                reject(err)
+            }
+
+            user.findOne({ '_id': decoded.userId, 'token': token })
+                .then(user => {
+                    resolve(user)
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    })
+}
+
+
 
 const User = mongoose.model('User', userSchema)
 
